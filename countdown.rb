f13f800eda3ec2130226195cc64c4c500f818c0e
Alexander Marusin <amarusin18@gmail.com>
2020-07-10 04:04:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
+minutes = 10
 
-def countdown
+def countdown(minutes)
+  while minutes > 0
+    puts "#{minutes} SECOND(S)!"
+    minutes -= 1
+  end 
+  return "HAPPY NEW YEAR!"
 end
